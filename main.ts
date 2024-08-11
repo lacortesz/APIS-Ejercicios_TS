630e8c78976332722f92961a47c3d6c16a32910f
lacortesz <la.cortesz1@uniandes.edu.co>
2024-08-11 11:46:30 -0500

    Ejercicio 3 - Commit 1

---
+++ main.ts
@@ -0,0 +1,5 @@
+import { Aprendiz, NivelEducativo } from "./aprendiz";
+
+export const ap = new Aprendiz("Luis Alberto", "Cortes Zambrano", "avatar.png", 30, NivelEducativo.POSGRADO);
+
+console.log(ap);
