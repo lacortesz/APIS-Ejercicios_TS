8f57f9bea5d140af0a423567600baaf630103cf2
lacortesz <la.cortesz1@uniandes.edu.co>
2024-08-11 12:13:33 -0500

    Ejercicio 3 - Ajustes

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-import { Aprendiz, NivelEducativo } from "./aprendiz";
+import { Aprendiz, NivelEducativo } from "./aprendiz.js";
 
 export const ap = new Aprendiz("Luis Alberto", "Cortes Zambrano", "avatar.png", 30, NivelEducativo.POSGRADO);
 
---
+++ scripts/main.js
@@ -0,0 +1,3 @@
+import { Aprendiz, NivelEducativo } from "./aprendiz.js";
+export var ap = new Aprendiz("Luis Alberto", "Cortes Zambrano", "avatar.png", 30, NivelEducativo.POSGRADO);
+console.log(ap);
